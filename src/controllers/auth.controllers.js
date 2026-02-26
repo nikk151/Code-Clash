4d3c77698565e6685527bc7143446bd911804e67
nikk151 <nikhilparihar151@gmail.com>
2026-02-26 12:35:57 +0530

    added login controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -59,11 +59,51 @@ async function registerController(req, res) {
 
 async function loginUser(req, res) {
 
+    const {username, email, password} = req.body;
+
+    const isUserExist = await userModel.findOne({
+        $or : [
+            {username},
+            {email}
+        ]
+    })
+
+    if (!isUserExist){
+        return res.status(400).json({
+            message: "User Not Found"
+        })
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, isUserExist.password)
+
+    if (!isPasswordMatched){
+        return res.status(400).json({
+            message: "Invalid Credentials"
+        })
+    }
+
+    const token = jwt.sign({id : isUserExist._id}, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    })
+
+    res.cookie("token", token, {httpOnly: true})
+
+    return res.status(200).json({
+        message: "User Logged In Successfully",
+        user: {
+            id: isUserExist._id,
+            username: isUserExist.username,
+            email: isUserExist.email,
+        }
+    })
+
+
 
 
 }
 
 
 module.exports = {
-    registerController
+    registerController,
+    loginUser
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { registerController } = require("../controllers/auth.controllers.js")
+const { registerController, loginUser } = require("../controllers/auth.controllers.js")
 
 const router = express.Router()
 
 
 router.post("/register", registerController)
+router.post("/login", loginUser)
 
 module.exports = router
 
